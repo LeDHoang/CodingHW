0f7370e30a276385e9b694955b749fac87ce4d7e
LeDHoang <87229058+LeDHoang@users.noreply.github.com>
2021-07-12 16:28:52 +0800

    Update Median.java

--- Median.java
+++ Median.java
@@ -44,11 +44,9 @@ return m;
  static int mostFrequent(int n, int arr[])
     {
          
-        // Sort the array
         Arrays.sort(arr);
          
-        // find the max frequency using linear
-        // traversal
+        
         int max_count = 1, res = arr[0];
         int curr_count = 1;
          
@@ -67,7 +65,7 @@ return m;
             }
         }
      
-        // If last element is most frequent
+        
         if (curr_count > max_count)
         {
             max_count = curr_count;
